ac9a42745176091bb48a9d35cd0b61438a73334c
Ademola Akindotuni <emricky17@gmail.com>
2023-02-01 13:16:46 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-named-as-default-member */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
