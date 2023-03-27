bf7c4dc3931e1b6292833d88d6acf4550e543594
Ademola Akindotuni <emricky17@gmail.com>
2023-03-27 01:15:24 +0100

    Add screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '5rem',
+			},
+		},
+		screens: {
+			xs: '360px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+		},
 		extend: {
 			colors: {
 				primary: {
@@ -39,6 +56,7 @@ module.exports = {
 					700: '#23272B',
 				},
 			},
+
 			boxShadow: {
 				'mui-button':
 					'0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)',
