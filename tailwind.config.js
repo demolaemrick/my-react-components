2c955dd281bb999d91759ca0ed78b8401f19ba62
Ademola Akindotuni <emricky17@gmail.com>
2023-04-09 20:50:04 +0100

    add font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,6 +56,9 @@ module.exports = {
 					700: '#23272B',
 				},
 			},
+			fontFamily: {
+				default: ['Poppins', 'sans-serif'],
+			},
 			boxShadow: {
 				'mui-button':
 					'0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)',
